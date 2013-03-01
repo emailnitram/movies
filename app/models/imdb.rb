bb6f5d0d9d95c7c8058ce536fdc0da94f431fd26
Martin Henry <emailnitram@gmail.com>
2013-03-01 15:37:31 -0800

    Changed method from controller to model

---
+++ app/models/imdb.rb
@@ -0,0 +1,9 @@
+class IMDBSearcher
+  def self.search(search)
+    raw_result = search || "man on fire"
+    result = raw_result.gsub(/\s+/, "+")
+    movie_details = HTTParty.get("http://imdbapi.org/?title="+result+"&type=json")
+
+    ActiveSupport::JSON.decode(movie_details)      
+  end
+end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,9 +1,4 @@
 class Search < ActiveRecord::Base
   attr_accessible :search
   
-  def search
-    
-    movie_details = HTTParty.get("http://imdbapi.org/?title=iron+man&type=json")
-    @searches = ActiveSupport::JSON.decode(movie_details)
-  end
 end
